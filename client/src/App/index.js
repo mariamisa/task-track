89c999b4a43aac0d63cc9c7133c224a307abf58a
mariam isa <maryaam1998@hotmail.com>
2021-10-14 14:49:26 +0300

    add theme

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,31 +1,38 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+import {
+  unstable_createMuiStrictModeTheme as uCreateMui,
+  ThemeProvider,
+} from '@mui/material/styles';
 
 import { DASHBOARD_PAGE, LOGIN_PAGE } from '../Utils/routes.constant';
 
 // import { LoggedOutRoutes, PrivateRoutes } from './Routes';
 
-// eslint-disable-next-line no-unused-vars
 import { Dashboard, Login, Layout, NotFound } from '../Pages';
 
 import './style.css';
 
+const theme = uCreateMui();
+
 const App = () => (
   <div className="App">
-    <Layout>
-      <Switch>
-        <Route exact path={DASHBOARD_PAGE}>
-          <Dashboard />
-        </Route>
-        <Route exact path={LOGIN_PAGE}>
-          <Login />
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Switch>
+          <Route exact path={DASHBOARD_PAGE}>
+            <Dashboard />
+          </Route>
+          <Route exact path={LOGIN_PAGE}>
+            <Login />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
+    </ThemeProvider>
   </div>
 );
 
