653de0fe3e439ebe367eec551c3219be740b34e7
mariam isa <maryaam1998@hotmail.com>
2021-10-15 22:47:17 +0300

    change endpoint name

--- server/router/user.js
+++ server/router/user.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const { isAuthController, updatePermissionController } = require('../controller');
 
 router.get('/is-auth', isAuthController);
-router.patch('/users/:id', updatePermissionController);
+router.patch('/users/permission/:id', updatePermissionController);
 
 module.exports = router;
