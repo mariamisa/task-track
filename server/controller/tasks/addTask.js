92ae3ccebeba1a0292d61f385ce14b49bb8e60e3
mariam isa <maryaam1998@hotmail.com>
2021-10-25 14:30:23 +0300

    add

--- server/controller/tasks/addTask.js
+++ server/controller/tasks/addTask.js
@@ -1,13 +1,23 @@
 const { addNewTask } = require('../../database/queries');
-const { boomify } = require('../../utils');
+const { boomify, addFilesToCloudenary } = require('../../utils');
 
 const addTask = async (req, res, next) => {
   try {
     const { add } = req.permission;
+    const { name, type } = req.body;
     if (!add) {
       throw boomify(401, 'you dont have permission to add task!');
     }
-    await addNewTask({ ...req.body });
+    const taskData = { name, type };
+    if (req.files && req.files.protocol && req.files.protocol.length > 0) {
+      const urls = await addFilesToCloudenary(req.files.protocol);
+      taskData.protocol = urls;
+    }
+    if (req.files && req.files.attach && req.files.attach.length > 0) {
+      const urls = await addFilesToCloudenary(req.files.attach);
+      taskData.attach = urls;
+    }
+    await addNewTask(taskData);
     res.status(200).json({
       statusCode: 200,
       msg: 'task added successfully',
