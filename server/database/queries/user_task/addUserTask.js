e8a4acd8f1839a83457ad503258260e0e061bddb
mariam isa <maryaam1998@hotmail.com>
2021-10-26 18:13:02 +0300

    edit

--- server/database/queries/user_task/addUserTask.js
+++ server/database/queries/user_task/addUserTask.js
@@ -1,11 +1,11 @@
 const connection = require('../../config/connection');
 
 const addUserTask = ({
-  date, deadline, visibility, userId, taskId,
+  date, deadline, visibility, status = 'onwork', userId, taskId,
 }) => {
   const sql = {
-    text: 'INSERT INTO tasks_users (date, deadline, visibility, user_id, task_id) VALUES ($1, $2, $3,$4,$5)',
-    values: [date, deadline, visibility, userId, taskId],
+    text: 'INSERT INTO tasks_users (date, deadline, visibility, status, user_id, task_id) VALUES ($1, $2, $3,$4,$5,$6)',
+    values: [date, deadline, visibility, status, userId, taskId],
   };
   return connection.query(sql);
 };
