a7eaaf5d01c6f18f576673a7a38e8959ecb4bfc2
mariamisa <mariam@kiitos-tech.com>
2021-11-02 22:13:23 +0200

    return user avatar

--- server/database/queries/user_task/getUsersTasks.js
+++ server/database/queries/user_task/getUsersTasks.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 const getUsersTasks = () => {
   const sql = {
-    text: 'SELECT users.username,users.mobile,users.id,users.country,tasks.id,tasks.name,users.job_title,tasks_users.status,tasks.* FROM tasks_users INNER JOIN users ON tasks_users.user_id=users.id INNER JOIN tasks ON tasks_users.task_id=tasks.id',
+    text: 'SELECT users.username,users.mobile,users.id,users.avatar,users.country,tasks.id,tasks.name,users.job_title,tasks_users.status,tasks.* FROM tasks_users INNER JOIN users ON tasks_users.user_id=users.id INNER JOIN tasks ON tasks_users.task_id=tasks.id',
   };
   return connection.query(sql);
 };
