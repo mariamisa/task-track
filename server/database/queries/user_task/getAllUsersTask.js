e295b8a669f7ba458849ba4f07575226ca1fbec3
mariam isa <47949938+mariamisa@users.noreply.github.com>
2021-11-03 07:52:41 +0200

    Return avatar with data

--- server/database/queries/user_task/getAllUsersTask.js
+++ server/database/queries/user_task/getAllUsersTask.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 const getUsersTasks = (taskId) => {
   const sql = {
-    text: 'SELECT users.username,users.mobile,users.id,users.country,tasks.id,tasks.name,users.job_title,tasks_users.user_id,tasks_users.status,tasks.* FROM tasks_users INNER JOIN users ON tasks_users.user_id=users.id INNER JOIN tasks ON tasks_users.task_id=tasks.id WHERE task_id=$1',
+    text: 'SELECT users.username,users.avatar,users.mobile,users.id,users.country,tasks.id,tasks.name,users.job_title,tasks_users.user_id,tasks_users.status,tasks.* FROM tasks_users INNER JOIN users ON tasks_users.user_id=users.id INNER JOIN tasks ON tasks_users.task_id=tasks.id WHERE task_id=$1',
     values: [taskId],
   };
   return connection.query(sql);
